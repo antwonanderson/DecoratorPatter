7c2916e7569ac4a02802abbf1cf52ce189034f9d
Antwon Anderson <antwonanderson@gmail.com>
2017-03-19 00:47:48 -0400

    Playing around with decorator pattern

---
+++ src/decoratorpattern/Beverage.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decoratorpattern;
+
+/**
+ *
+ * @author antwonanderson
+ */
+public abstract class Beverage {
+    
+    String description = "Unknow Beverage";
+
+    public String getDescription() {
+        return description;
+    }
+    
+    public abstract double cost();
+    
+}
---
+++ src/decoratorpattern/DarkRoast.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decoratorpattern;
+
+/**
+ *
+ * @author antwonanderson
+ */
+public class DarkRoast extends Beverage {
+    
+    public DarkRoast(){
+        description = "DarkRoast";
+    }
+    
+    @Override
+    public double cost() {
+        return .89;
+    }
+}
